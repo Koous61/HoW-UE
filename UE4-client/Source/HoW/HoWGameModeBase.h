2d89f481881dba658f6188c5332c275a09141250
koous <dima_nichiporchik@mail.ru>
2020-02-06 18:35:33 +0300

    update

---
+++ UE4-client/Source/HoW/HoWGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoWGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOW_API AHoWGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
